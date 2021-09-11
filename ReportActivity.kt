fcbbb220dd77360ce51c5ae39b4bf7762de8d0ed
witness nkge <79025245+Witness-nkge@users.noreply.github.com>
2021-09-12 01:39:16 +0200

    Add files via upload

---
+++ ReportActivity.kt
@@ -0,0 +1,12 @@
+package com.wintech.diydr
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.wintech.diydr.R
+
+class ReportActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_report)
+    }
+}
